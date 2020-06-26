0ee8d9ca5535375740d2d992a897ce08af919f11
Adam Sebesta <adam.sebesta5@gmail.com>
2020-06-26 18:43:24 +0200

    cont working on login logic

--- spa/src/components/Metamask.vue
+++ spa/src/components/Metamask.vue
@@ -21,47 +21,49 @@
 </template>
 
 <script>
-    import Web3 from 'web3';
-    export default {
-        name: "Metamask",
-        data() {
-            return {
-                currentAddress: "",
-                hover: false
-            }
-        },
-        methods: {
-          async loadWeb3() {
-            // Modern dapp browsers...
-            if (window.ethereum) {
-              window.web3 = new Web3(window.ethereum);
-              await window.ethereum.enable()
-            }
-              // Legacy dapp browsers...
-            else if (window.web3) {
-              window.web3 = new Web3(window.web3.currentProvider);
-            }
-            // Non-dapp browsers...
-            else {
-              window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
-            }
-          },
-          async loadBlockchainData() {
-            const web3 = window.web3
-            const accounts = await web3.eth.getAccounts()
-            this.currentAddress = accounts[0]
-          },
-          loginAndSetAddress() {
-            this.loadWeb3();
-            this.loadBlockchainData();
-          }
-        },
-        created() {
-          this.loadWeb3();
-          this.loadBlockchainData();
-        },
+  import Web3 from 'web3';
+  export default {
+    name: "Metamask",
+    data() {
+      return {
+        currentAddress: "",
+        hover: false
+      }
+    },
+    methods: {
+      async loadWeb3() {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+          window.web3 = new Web3(window.ethereum);
+          await window.ethereum.enable()
+        }
+          // Legacy dapp browsers...
+        else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider);
+        }
+        // Non-dapp browsers...
+        else {
+          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+      },
+      async loadBlockchainData() {
+        const web3 = window.web3
+        const accounts = await web3.eth.getAccounts()
+        this.currentAddress = accounts[0]
+        console.log("hi");
 
-    }
+      },
+      loginAndSetAddress() {
+        this.loadWeb3();
+        this.loadBlockchainData();
+
+      }
+    },
+    created() {
+      this.loadWeb3();
+      this.loadBlockchainData();
+    },
+  }
 </script>
 
 <style scoped>
