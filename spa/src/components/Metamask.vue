999b71e356e75792c7be65bffd064bbe6e39aaea
Adam Sebesta <adam.sebesta5@gmail.com>
2020-06-26 21:14:08 +0200

    working on sign in logic for 1st visit

--- spa/src/components/Metamask.vue
+++ spa/src/components/Metamask.vue
@@ -26,27 +26,27 @@
     name: "Metamask",
     data() {
       return {
-        currentAddress: "",
+        currentAddress: null,
         hover: false
       }
     },
     methods: {
       async loadWeb3() {
         // Modern dapp browsers...
-        if (window.ethereum) {
-          window.web3 = new Web3(window.ethereum);
-          await window.ethereum.enable()
-          this.loadBlockchainData();
-        }
-          // Legacy dapp browsers...
-        else if (window.web3) {
-          window.web3 = new Web3(window.web3.currentProvider);
-        }
-        // Non-dapp browsers...
-        else {
-          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        }
-      },
+          if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable()
+            this.loadBlockchainData();
+          }
+            // Legacy dapp browsers...
+          else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider);
+          }
+          // Non-dapp browsers...
+          else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+          }
+        },
       async loadBlockchainData() {
         const web3 = window.web3
         const accounts = await web3.eth.getAccounts()
@@ -57,8 +57,8 @@
       }
     },
     created() {
-      this.loadWeb3();
-    },
+      this.loadBlockchainData();
+    }
   }
 </script>
 
