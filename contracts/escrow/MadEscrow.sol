cd7173bb5915af370d6f89d2d7b53517a4a70856
RobinD57 <robin.davidseth@gmail.com>
2020-07-06 13:25:31 +0200

    changed config, updates imported contracts and removed unnecessary ones

--- contracts/escrow/MadEscrow.sol
+++ contracts/escrow/MadEscrow.sol
@@ -2,17 +2,7 @@ pragma solidity ^0.6.0;
 
 import "../installed_contracts/zeppelin/contracts/utils/Address.sol";
 
-/**
- * @title ConditionalEscrow
- * @dev Base abstract escrow to only allow withdrawal if a condition is met.
- * @dev Intended usage: See {Escrow}. Same usage guidelines apply here.
- */
 abstract contract ConditionalEscrow is Escrow {
-    /**
-     * @dev Returns whether an address is allowed to withdraw their funds. To be
-     * implemented by derived contracts.
-     * @param payee The destination address of the funds.
-     */
     AuthencityOracleInterface private oracleInstance;
     address private oracleAddress;
     bool private authenticityCheck;
