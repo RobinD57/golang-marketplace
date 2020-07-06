e83ffd55458cb15c9c7a8e6a5c7b377b52214934
RobinD57 <robin.davidseth@gmail.com>
2020-07-06 10:58:27 +0200

    added oracle call in escrow smart contract

---
+++ contracts/AuthentictyOracleInterface.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.6.0;
+contract AuthenticityOracleInterface {
+  function authentictyCheck() public returns (uint256);
+}
--- contracts/MadEscrow.sol
+++ contracts/MadEscrow.sol
@@ -13,6 +13,35 @@ abstract contract ConditionalEscrow is Escrow {
      * implemented by derived contracts.
      * @param payee The destination address of the funds.
      */
+    AuthencityOracleInterface private oracleInstance;
+    address private oracleAddress;
+    bool private authenticityCheck;
+    mapping(uint256=>bool) myRequests;
+    event AuthencityCheckCompletedEvent(bool authenticityCheck, uint256 id);
+    event ReceivedNewRequestIdEvent(uint256 id);
+
+    function setOracleAddress(address _oracleAddress) public onlyOwner {
+            oracleAddress = _oracleAddress;
+    }
+
+    function updateAuthenticityCheck() public {
+      uint256 id = oracleInstance.authentictyCheck();
+      myRequests[id] = true;
+      emit ReceivedNewRequestIdEvent(id);
+    }
+
+    function callback(bool _authenticityCheck, uint256 _id) public onlyOracle {
+      require(myRequests[_id], "Request not in pending list.");
+      authenticityCheck = _authenticityCheck;
+      delete myRequests[_id];
+      emit AuthencityCheckCompletedEvent(_authenticityCheck, _id);
+    }
+
+    modifier onlyOracle() {
+     require(msg.sender == oracleAddress, "You are not authorized to call this function.");
+      _;
+    }
+
     function withdrawalAllowed(address payee) public view virtual returns (bool);
 
     function withdraw(address payable payee) public virtual override {
