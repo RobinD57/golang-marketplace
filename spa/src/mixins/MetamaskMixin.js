3d5cc95725c565af457130eeefec5618a90b8a27
Adam Sebesta <adam.sebesta5@gmail.com>
2020-07-09 14:09:24 +0200

    refactored loadBlockchain Data to private async function

--- spa/src/mixins/MetamaskMixin.js
+++ spa/src/mixins/MetamaskMixin.js
@@ -6,7 +6,8 @@ export default {
         if (window.ethereum) {
           window.web3 = new Web3(window.ethereum);
           await window.ethereum.enable();
-          this.loadBlockchainData();
+          const comp = this;
+          loadBlockchainData(comp);
         }
           // Legacy dapp browsers...
         else if (window.web3) {
@@ -17,15 +18,15 @@ export default {
           window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
         }
       },
-    async loadBlockchainData() {
-      const web3 = window.web3
-      const accounts = await web3.eth.getAccounts()
-      this.currentAddress = accounts[0];
-    },
     loginAndSetAddress() {
       if (!this.currentAddress) {
         this.loadWeb3();
       }
     }
   }
+}
+async function loadBlockchainData(comp) {
+  const web3 = window.web3
+  const accounts = await web3.eth.getAccounts()
+  comp.currentAddress = accounts[0];
 }
